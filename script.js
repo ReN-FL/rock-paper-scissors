28ae323f2f2ce6739decb8c38b3bc1b3eadb86f1
ReN <132862627+ReNlapf10@users.noreply.github.com>
2024-01-29 21:31:11 -0300

    Add playGame() and playRound()
    the playRound function compare the player choice
    and the computer choice and checks who won.
    the playGame function gets the player input,
    loop the playRound() 5 times store scores and checks who
    won the game

--- script.js
+++ script.js
@@ -12,3 +12,55 @@ function getComputerChoice() {
     return choice;
   }
 }
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  if (playerSelection === computerSelection) {
+    return 'its a tie ';
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    return `Player win, ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `Computer win, ${computerSelection} beats ${playerSelection}`;
+  }
+}
+function playGame() {
+  let i = 0;
+  let playerScore = 0;
+  let computerScore = 0;
+  while (i < 5) {
+    const computerChoice = getComputerChoice();
+    const playerChoice = prompt('paper, rock or scissors?');
+    let roundResult = playRound(playerChoice, computerChoice);
+    console.log(roundResult);
+    if (roundResult.substring(0, 11) == 'Player win,') {
+      playerScore++;
+      console.log('Player Score', playerScore);
+    } else if (roundResult.substring(0, 13) == 'Computer win,') {
+      computerScore++;
+      console.log('Computer Score', computerScore);
+    } else {
+    }
+    i++;
+  }
+  if (playerScore > computerScore) {
+    console.log(
+      `Final Score, Player: ${playerScore} Computer: ${computerScore}`
+    );
+    return 'Player Won the game';
+  } else if (computerScore > playerScore) {
+    console.log(
+      `Final Score, Player: ${playerScore} Computer: ${computerScore}`
+    );
+    return 'Computer Won the game';
+  } else {
+    console.log(
+      `Final Score, Player: ${playerScore} Computer: ${computerScore}`
+    );
+    return 'its a tie, No one won :(';
+  }
+}
+console.log(playGame());
